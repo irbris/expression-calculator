962a7f8e56696629fe731eca571f5b965dcb065f
irbris <55032489+irbris@users.noreply.github.com>
2019-09-26 23:29:41 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -4,13 +4,38 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-  var str = expr.replace(/\s/g, '')
+  var str = expr.replace(/\s/g, '');
+  var braskets = str.match(/[\(\)]/g);
+  if(braskets) {
+    var num = 0;
+    braskets.map(element => {
+      if(element === '(') {
+        num ++;
+      } if(element === ')') {
+        num --;
+        if(num < 0) {
+          throw 'ExpressionError: Brackets must be paired'
+        }
+      }
+    });
+    if(num !== 0) {
+      throw 'ExpressionError: Brackets must be paired'
+    }
+  }
+
   var value = str.match(/(^[0-9*\/\\(\\)+-]+$)/);
-  var res = new Function('return '+ value );
-  return +res().toFixed(4);
+  var resalt = new Function('return '+ value );
+  var res = +resalt().toFixed(4);
+  if(res === Infinity) {
+    throw 'TypeError: Devision by zero.'
+  }
+  
+  return res;
 }
 
 
 module.exports = {
     expressionCalculator
 }
+
+// https://toster.ru/q/516733
