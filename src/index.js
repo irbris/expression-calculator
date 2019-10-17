1b246cf3616e54382b84095d0889d8ae6e6d6401
irbris <55032489+irbris@users.noreply.github.com>
2019-10-17 14:52:28 +0300

    fix: remowe new from solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
-function expressionCalculator(expr) {
-  var str = expr.replace(/\s/g, '');
-  var braskets = str.match(/[\(\)]/g);
+const calculateTwoNumber = {
+  '*': (a, b) => a * b,
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '/': (a, b) => a / b
+};
+
+const checkBrackets = (expr) => {
+  const braskets = expr.match(/[\(\)]/g);
   if(braskets) {
-    var num = 0;
+    let num = 0;
     braskets.map(element => {
       if(element === '(') {
         num ++;
@@ -22,20 +28,64 @@ function expressionCalculator(expr) {
       throw 'ExpressionError: Brackets must be paired'
     }
   }
+};
 
-  var value = str.match(/(^[0-9*\/\\(\\)+-]+$)/);
-  var resalt = new Function('return '+ value );
-  var res = +resalt().toFixed(4);
-  if(res === Infinity) {
-    throw 'TypeError: Devision by zero.'
-  }
-  
-  return res;
+const parseString = (expr) => {
+  const operandList = expr.split(/(?<=[-+*/()])|(?=[-+*/()])/g)
+          .map(el => isNaN(parseInt(el)) ? el : parseInt(el));
+  return operandList;
 }
 
+const calculateParseString = (expr) => {
+  let result = null;
+  for (let i = 0; i < expr.length; i++) {
+    if (expr[i] === '/' && expr[i + 1] === 0) {
+      throw 'TypeError: Division by zero.'
+    } 
+    if (expr[i] === '/' || expr[i] === '*') {
+      result = calculateTwoNumber[expr[i]](expr[i - 1], expr[i + 1])
+      expr.splice(i - 1, 3, result);
+      i -= 1;
+    }
+  }
+  result = expr[0];
+  for (let i = 0; i < expr.length; i++) {
+    if (expr[i] === '+' || expr[i] === '-') {
+      result = calculateTwoNumber[expr[i]](expr[i - 1], expr[i + 1])
+      expr.splice(i - 1, 3, result);
+      i -= 1;
+    }
+  }
+  return result;
+};
+
+const calculateWithBrackets = (expr) => {
+  let openingBrasket = null;
+  let closingBrasket = null;
+
+  for (let i = 0; i < expr.length; i++) {
+    if (expr[i] === '(') {
+      openingBrasket = i;
+    } else if (expr[i] === ')') {
+      closingBrasket = i;
+      const res = calculateParseString(expr.splice(openingBrasket + 1, closingBrasket - openingBrasket - 1));
+      expr.splice(openingBrasket, 2, res);
+      openingBrasket = null;
+      closingBrasket = null;
+      i = 0;
+    } 
+  }
+  return calculateParseString(expr);
+};
+
+function expressionCalculator(expr) {
+  checkBrackets(expr);
+  let exprNoSpace = expr.replace(/\s/g, '');
+  let exprToString = parseString(exprNoSpace);
+  let exprCalc = calculateWithBrackets(exprToString); 
+  return exprCalc;
+}
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
-
-// https://toster.ru/q/516733
